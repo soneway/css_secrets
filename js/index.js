ace93c99309e6a847a6e244a3fd11c74a1e9c3a7
soneway <250084439@qq.com>
2015-09-06 17:55:10 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+﻿var $ = require('jq'),
+    $doc = $(document);
+
+var navAs = $('nav a');
+$doc.on('click', 'nav a', function () {
+    navAs.removeClass('selected');
+    $(this).addClass('selected');
+});
